3e53640bde11b8ce4b956bd9d6cc73d6d03c3fbd
Marcin Czerwinski <marbroczerwinski@gmail.com>
2023-12-08 16:07:11 +0100

    Solve 7th day

--- solver/solutions/day_06/solve.py
+++ solver/solutions/day_06/solve.py
@@ -1,5 +1,6 @@
 from solver.utils.file_utils import *
 import os
+import re
 
 def solve(type, part):
   target_data_file = 'test.txt' if type == 'test' else 'data.txt'
@@ -15,7 +16,49 @@ def do_solve(part, data):
     return solve_second_part(data)
 
 def solve_first_part(data):
-  pass
+  formatted_data = format_data(data)
+  return count_winning_way_number(formatted_data)
 
 def solve_second_part(data):
-  pass
+  formatted_data = format_data_second_part(data)
+  return count_winning_way_number(formatted_data)
+
+def format_data(data):
+  [time_line, distance_line] = data
+  times = [int(x) for x in re.findall(r'[0-9]+', time_line)]
+  distances = [int(x) for x in re.findall(r'[0-9]+', distance_line)]
+  formatted_data = []
+  for i in range(len(times)): {
+    formatted_data.append({'time': times[i], 'distance': distances[i]})
+  }
+  return formatted_data
+
+def format_data_second_part(data):
+  [time_line, distance_line] = data
+  times = [x for x in re.findall(r'[0-9]+', time_line)]
+  distances = [x for x in re.findall(r'[0-9]+', distance_line)]
+  time = int(''.join(times))
+  distance = int(''.join(distances))
+  return [{'time': time, 'distance': distance}]
+
+def count_winning_way_number(data):
+  winning_ways = []
+  for d in data:
+    result = count_winning_possibilities(d['time'], d['distance'])
+    winning_ways.append(result)
+
+  multiplied_ways = None
+  for way in winning_ways:
+    if multiplied_ways == None:
+      multiplied_ways = way
+    else:
+      multiplied_ways *= way
+  return multiplied_ways
+
+def count_winning_possibilities(time, distance):
+  winning_possibilities = 0
+  for t in range(time+1):
+    distance_reached = t * (time - t)
+    if distance_reached > distance:
+      winning_possibilities += 1
+  return winning_possibilities
