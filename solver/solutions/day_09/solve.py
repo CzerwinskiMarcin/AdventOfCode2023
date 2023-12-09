8ee914b1e24f3b8d1bf52e9a7a8aab77335c3b73
Marcin Czerwinski <marbroczerwinski@gmail.com>
2023-12-09 23:48:03 +0100

    Solve 9th day

--- solver/solutions/day_09/solve.py
+++ solver/solutions/day_09/solve.py
@@ -15,7 +15,68 @@ def do_solve(part, data):
     return solve_second_part(data)
 
 def solve_first_part(data):
-  pass
+  formatted_data = format_data(data)
+  [update_differences(numbers) for numbers in formatted_data]
+  [extrapolate_forward(numbers) for numbers in formatted_data]
+
+  return sum_extrapolates_forward(formatted_data)
 
 def solve_second_part(data):
-  pass
+  formatted_data = format_data(data)
+  [update_differences(numbers) for numbers in formatted_data]
+  [extrapolate_backward(numbers) for numbers in formatted_data]
+
+  return sum_extrapolates_backward(formatted_data)
+
+def format_data(data):
+  sensors_data = []
+  for line in data:
+    numbers = [int(x) for x in line.split(' ')]
+    sensors_data.append([numbers])
+  return sensors_data
+
+def update_differences(numbers):
+  are_all_differences_equal_zero = False
+
+  while not are_all_differences_equal_zero:
+    differences = []
+
+    for i in range(len(numbers[-1]) - 1):
+      differences.append(numbers[-1][i+1] - numbers[-1][i])
+    
+    are_all_differences_equal_zero = all([number == 0 for number in differences])
+    numbers.append(differences)
+  
+def extrapolate_forward(numbers):
+  numbers.reverse()
+
+  last_predict = 0
+  for i in range(len(numbers) - 1):
+    last_number = numbers[i+1][-1]
+    last_predict = last_number + last_predict
+    numbers[i+1].append(last_predict)
+  
+  numbers.reverse()
+
+def extrapolate_backward(numbers):
+  numbers.reverse()
+
+  last_predict = 0
+  for i in range(len(numbers) - 1):
+    last_number = numbers[i+1][0]
+    last_predict = last_number - last_predict
+    numbers[i+1].insert(0, last_predict)
+  
+  numbers.reverse()
+
+def sum_extrapolates_forward(data):
+  sum = 0
+  for numbers in data:
+    sum += numbers[0][-1]
+  return sum
+
+def sum_extrapolates_backward(data):
+  sum = 0
+  for numbers in data:
+    sum += numbers[0][0]
+  return sum
